165487573d6dedf47ea3d5c644718753e90ef5fc
ShuliTalOr <shulital20@gmail.com>
2022-08-12 17:08:29 +0300

    create_user_fragment

--- app/src/main/java/com/example/myagent/MainActivity.java
+++ app/src/main/java/com/example/myagent/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.myagent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile ="sharedPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//        FragmentManager fm = getFragmentManager().
 
     }
 
