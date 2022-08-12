9d576d210e97bea3d67e7266b0150adadb8aa956
ShuliTalOr <shulital20@gmail.com>
2022-08-12 17:55:21 +0300

    create_user_fragment.

--- app/src/main/java/com/example/myagent/MainActivity.java
+++ app/src/main/java/com/example/myagent/MainActivity.java
@@ -2,21 +2,26 @@ package com.example.myagent;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import com.example.myagent.agentPages.CreateNewUserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences mPreferences;
     private String sharedPrefFile ="sharedPreferences";
-
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FragmentManager fm = getFragmentManager().
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_activity, new CreateNewUserFragment()).commit();
 
     }
 
