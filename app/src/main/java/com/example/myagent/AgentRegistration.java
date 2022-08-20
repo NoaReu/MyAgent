14d4e1eb79f5b20e2fee395ddd9cea2542b56011
ShuliTalOr <shulital20@gmail.com>
2022-08-20 12:39:40 +0300

    new_instance_request_item.xml added

--- app/src/main/java/com/example/myagent/AgentRegistration.java
+++ app/src/main/java/com/example/myagent/AgentRegistration.java
@@ -1,4 +1,4 @@
-package com.example.myagent.userPages;
+package com.example.myagent;
 
 import android.os.Bundle;
 
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.myagent.R;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NewInsuranceRequestAtCustomer#newInstance} factory method to
+ * Use the {@link AgentRegistration#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewInsuranceRequestAtCustomer extends Fragment {
+public class AgentRegistration extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class NewInsuranceRequestAtCustomer extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NewInsuranceRequestAtCustomer() {
+    public AgentRegistration() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class NewInsuranceRequestAtCustomer extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewInsuranceRequestAtCustomer.
+     * @return A new instance of fragment AgentRegistration.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewInsuranceRequestAtCustomer newInstance(String param1, String param2) {
-        NewInsuranceRequestAtCustomer fragment = new NewInsuranceRequestAtCustomer();
+    public static AgentRegistration newInstance(String param1, String param2) {
+        AgentRegistration fragment = new AgentRegistration();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +59,6 @@ public class NewInsuranceRequestAtCustomer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_insurance_request_at_customer, container, false);
+        return inflater.inflate(R.layout.fragment_agent_registration, container, false);
     }
 }
