c956e2c52223355facf3a6522a169e60accbf8ed
ShuliTalOr <shulital20@gmail.com>
2022-08-20 14:39:14 +0300

    new_instance_request_item.xml added

--- app/src/main/java/com/example/myagent/agentPages/SearchCustomerAtAgent.java
+++ app/src/main/java/com/example/myagent/agentPages/SearchCustomerAtAgent.java
@@ -61,6 +61,6 @@ public class SearchCustomerAtAgent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sesrch_customer_at_agent, container, false);
+        return inflater.inflate(R.layout.fragment_search_customer_at_agent, container, false);
     }
 }
