e3cd80db7dd1fba5cf0d3829b9cc4bc90001d8ff
ShuliTalOr <shulital20@gmail.com>
2022-12-17 20:54:36 +0200

    .

--- app/src/main/java/com/example/myagent/agentPages/AgentSuitsList.java
+++ app/src/main/java/com/example/myagent/agentPages/AgentSuitsList.java
@@ -1,12 +1,17 @@
 package com.example.myagent.agentPages;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.myagent.R;
 import com.example.myagent.objects.Document;
@@ -29,6 +34,7 @@ public class AgentSuitsList extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    RecyclerView recyclerView;
 
     public AgentSuitsList() {
         // Required empty public constructor
@@ -66,12 +72,63 @@ public class AgentSuitsList extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_agent_suits_list, container, false);
-
+        recyclerView = view.findViewById(R.id.new_suits_at_agent_recycler_view);
         List<Document> documents = new ArrayList<>();
         documents.add(new Document("066465238","suit_035856038_28_09_22.pdf","חדש", "אפי טלאור","035856038", "gs://myagent-6cce7.appspot.com/066465238/035856038"));
         documents.add(new Document("066465238","suit_053974705_28_09_22.pdf","חדש", "אבי כהן","053974705","gs://myagent-6cce7.appspot.com/066465238/053974705"));
 
 
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(new DocAdapter(getContext(),documents));
+
+
+
+
         return view;
     }
+
+
+    public class DocViewHolder extends RecyclerView.ViewHolder{
+
+        TextView docName;
+        TextView docStatus;
+
+
+        public DocViewHolder(@NonNull View itemView){
+            super(itemView);
+            docName= itemView.findViewById(R.id.document_name_at_doc_item);
+            docStatus= itemView.findViewById(R.id.document_status_at_doc_item);
+
+        }
+    }
+
+    public class DocAdapter extends RecyclerView.Adapter<DocViewHolder>{
+
+        Context context;
+        List<Document> documents;
+
+        public DocAdapter(Context context, List<Document> documents){
+            this.context=context;
+            this.documents=documents;
+        }
+        @NonNull
+        @Override
+        public DocViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new DocViewHolder(LayoutInflater.from(context).inflate(R.layout.document_list_item,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DocViewHolder holder, int position) {
+
+            holder.docName.setText(documents.get(position).getDocumentName());
+            holder.docStatus.setText(documents.get(position).getStatus());
+        }
+
+        @Override
+        public int getItemCount() {
+            return documents.size();
+        }
+    }
+
 }
